bf608ffe0a8694bb14bb57a9f9e2798d6adebbac
AkoruuDev <akoruu.dev@gmail.com>
2023-07-04 14:06:50 -0300

    feat: update Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import { CommingSoon } from "../services/commingSoon/CommingSoon";
-import { Button, Container } from "../styles";
+import { CommingSoon } from "../services/CommingSoon";
+import { Button, Container, Space } from "../styles";
 import { AiOutlineMenu } from 'react-icons/ai';
 import logo from '../assets/new-logo.png';
 import { styled } from "styled-components";
 import { IconContext } from "react-icons";
+import { Footer } from "./Footer";
 
 function Home() {
     const wait = false;
@@ -15,7 +16,7 @@ function Home() {
             {
                 wait ?
                 <CommingSoon /> :
-                <Container>
+                <ContainerHome>
                     <Nav>
                         <div>
                             <IconContext.Provider value={{ color: "#f7f7f7", size: "50px", style: { cursor: "pointer" } }}>
@@ -27,7 +28,7 @@ function Home() {
                             </Login>
                         </div>
                     </Nav>
-                    <section>
+                    <SecOne>
                         <div>
                             <img src={logo} alt="rinhuix logo" />
                             <Title>
@@ -37,12 +38,34 @@ function Home() {
                                 </p>
                             </Title>
                         </div>
-                    </section>
-                </Container>
+                    </SecOne>
+                    <SecTwo>
+                        <div>
+                            <h1>Hear your called</h1>
+                            <Space />
+                            <p>
+                                Imagine that all your life was only a dream and NOW you waked up
+                            </p>
+                            <p>
+                                All your turn is new and fantastic with dragons and magic and you are one their
+                            </p>
+                            <Space />
+                            <p>Explore a new world and meet new people</p>
+                            <p>Explore dungeons and recive items</p>
+                            <p>Make a perfect world to you</p>
+                        </div>
+                    </SecTwo>
+                    <Footer />
+                </ContainerHome>
             }
         </div>
     )
 }
+
+const ContainerHome = styled(Container)`
+    justify-content: flex-start;
+    padding-top: 18vh;
+`
 
 const Nav = styled.nav`
     position: fixed;
@@ -72,10 +95,17 @@ const Login = styled.div`
     user-select: none;
 `;
 
-const Title = styled.div`
+const Section = styled.section`
+    width: 100vw;
     display: flex;
-    flex-direction: column;
-    text-align: center;
+    justify-content: center;
+    & div {
+        width: 80%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 
     h1 {
         font-family: 'Bruno Ace SC', cursive;
@@ -84,6 +114,22 @@ const Title = styled.div`
     p {
         font-family: 'Julius Sans One', sans-serif;
     }
+`
+
+const SecOne = styled(Section)`
+    margin-bottom: 100px;
+`
+
+const SecTwo = styled(Section)`
+    background-color: #303030;
+    border-radius: 50px 50px 0 0;
+    padding: 40px 0;
+`
+
+const Title = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
 `
 
 export {
