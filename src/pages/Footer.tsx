4da4bbac0fdb9778f32d03682294686ea136ffec
AkoruuDev <akoruu.dev@gmail.com>
2023-07-04 14:06:26 -0300

    feat: create footer

---
+++ src/pages/Footer.tsx
@@ -0,0 +1,93 @@
+import { styled } from "styled-components"
+import { Space } from "../styles"
+import { useNavigate } from "react-router-dom"
+
+function Footer() {
+    const navigate = useNavigate()
+
+    return (
+        <Bottom>
+            <div>
+                <Infos>
+                    <div>
+                        <p onClick={() => navigate('/sign-in')}>sign-in</p>
+                        <p onClick={() => navigate('/sign-up')}>sign-up</p>
+
+                        <p onClick={() => navigate('/library')}>library</p>
+                        <p onClick={() => navigate('/systems')}>systems</p>
+                        <p onClick={() => navigate('/cards')}>cards</p>
+                    </div>
+                    <div>
+                        <p onClick={() => navigate('/new/char')}>new character</p>
+                        <p onClick={() => navigate('/new/camp')}>new campaign</p>
+                        <p onClick={() => navigate('/roll')}>roll</p>
+                        <p onClick={() => navigate('/maps')}>maps</p>
+                    </div>
+                    <div>
+                        <p onClick={() => navigate('/profile/me')}>profile</p>
+                        <p onClick={() => navigate('/profile/me/cards')}>my cards</p>
+                        <p onClick={() => navigate('/profile/me/char')}>my characters</p>
+                        <p onClick={() => navigate('/profile/me/camp')}>my campaigns</p>
+                    </div>
+                    <div>
+                        <p onClick={() => navigate('/about-us')}>about us</p>
+                        <p onClick={() => navigate('/news')}>news</p>
+                        <p onClick={() => navigate('/store')}>store</p>
+                    </div>
+                </Infos>
+            
+                <Space />
+
+                <Tm>
+                    <span>Rinhuix &copy; 2021 - All rights reserved</span>
+                    <span>Created by Akoruu Dev</span>
+                </Tm>
+            </div>
+        </Bottom>
+    )
+};
+
+const Bottom = styled.footer`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+
+    padding: 40px 0;
+    background-color: #1f1f1f;
+    color:#fff;
+    font-size:.8rem;
+
+    & > div {
+        width: 80%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    span {
+        font-family: 'Bruno Ace SC', cursive;
+    }
+`
+
+const Infos = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    font-weight: 700;
+
+    p {
+        margin-bottom: 18px;
+        cursor: pointer;
+    }
+`
+
+const Tm = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export {
+    Footer,
+}
