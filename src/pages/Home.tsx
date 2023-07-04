d791baad9b9530e30f75cbdbfe92a27cabee6108
AkoruuDev <akoruu.dev@gmail.com>
2023-07-03 22:48:46 -0300

    chore: start create Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,26 +24,6 @@ function Home() {
                     <section>
                         <img src={logo} alt="rinhuix logo" />
                     </section>
-                    {/* <Button onClick={() => navigate('/')}>Home</Button>
-                    <Button onClick={() => navigate('/sign-in')}>SignIn</Button>
-                    <Button onClick={() => navigate('/sign-up')}>SignUp</Button>
-                    <Button onClick={() => navigate('*')}>PageNotFound</Button>
-
-                    <Button onClick={() => navigate('/library')}>Library</Button>
-                    <Button onClick={() => navigate('/systems')}>Systems</Button>
-                    <Button onClick={() => navigate('/roll')}>Roll</Button>
-                    <Button onClick={() => navigate('/maps')}>Maps</Button>
-                    <Button onClick={() => navigate('/about-us')}>AboutUs</Button>
-                    <Button onClick={() => navigate('/news')}>News</Button>
-                    <Button onClick={() => navigate('/store')}>Store</Button>
-                    <Button onClick={() => navigate('/cards')}>Cards</Button>
-                    <Button onClick={() => navigate('/new/char')}>NewChar</Button>
-                    <Button onClick={() => navigate('/new/camp')}>NewCamp</Button>
-                    
-                    <Button onClick={() => navigate('/profile/me')}>Me</Button>
-                    <Button onClick={() => navigate('/profile/me/cards')}>MyCards</Button>
-                    <Button onClick={() => navigate('/profile/me/char')}>MyChars</Button>
-                    <Button onClick={() => navigate('/profile/me/camp')}>MyCamp</Button> */}
                 </Container>
             }
         </div>
